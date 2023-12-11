a046949a9706692880276f7952a97a830898481f
Jacob Quigley <quigley.jacob3@icloud.com>
2023-12-10 23:06:13 -0500

    Fix build issues

--- src/components/guild/DatacronChecklist.js
+++ src/components/guild/DatacronChecklist.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Header, Grid, List, Form, Icon, Button, Input, Ref, Dropdown, Item, GridColumn } from 'semantic-ui-react';
+import { Header, Grid, List, Form, Icon, Button, Input, Ref, GridColumn } from 'semantic-ui-react';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 import { getDatacronTests, updateDatacronTests, defaultGuildChecklistState } from '../../server/datacrons';
 import { isValidDatacronTest, getSetFromTest } from '../../utils/datacrons';
@@ -22,7 +22,7 @@ function DatacronChecklist({redirect, guild, isOfficer, datacrons, session, disp
 		})()
 	}, [redirect, guild.id, session, displayMessage])
 
-    const updateGuildDatacronChecklist = () => {
+    const updateGuildDatacronChecklist = useCallback(() => {
         let newGuildDatacronTest = JSON.parse(JSON.stringify(guildDatacronTest))
         Object.keys(newGuildDatacronTest).forEach(key => {
             let datacronList = newGuildDatacronTest[key].list
@@ -33,11 +33,11 @@ function DatacronChecklist({redirect, guild, isOfficer, datacrons, session, disp
             })
         })
         setGuildDatacronTest(newGuildDatacronTest)
-    }
+    }, [datacron, guildDatacronTest])
 
     useEffect(() => {
         updateGuildDatacronChecklist()
-    }, [datacron])
+    }, [datacron, updateGuildDatacronChecklist])
 
     function guidGenerator() {
         var S4 = function() {
