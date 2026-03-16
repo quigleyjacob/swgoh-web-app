34b7c546b4468ad70f2fbf8d38379ca838828148
Jacob Quigley <quigley.jacob3@icloud.com>
2026-03-16 12:06:47 -0400

    some more cleanup

--- src/components/gac/GacOffense.js
+++ src/components/gac/GacOffense.js
@@ -269,14 +269,15 @@ function GacOffense ({account, opponent, active, setActive, categories, units, a
 					member += `${baseId}%2C`
 				}
 			})
-			if(member.length > 0) {
+			let useMember = member.length > 0
+			let useReinforce = reinforcement.length > 0
+			if(useMember) {
 				member = member.slice(0, -3)
 			}
-			if(reinforcement.length > 0) {
+			if(useReinforce) {
 				reinforcement = reinforcement.slice(0, -3)
 			}
-			let useReinforce = reinforcement.length > 0
-			window.open(`${url}/${leader}?d_member=${member}${useReinforce ? `&d_reinforcement=${reinforcement}`: ''}`, '_blank')
+			window.open(`${url}/${leader}${useMember || useReinforce ? '?' : ''}${useMember ? `d_member=${member}` : ''}${useReinforce ? `&d_reinforcement=${reinforcement}`: ''}`, '_blank')
 		}
 	}
 
